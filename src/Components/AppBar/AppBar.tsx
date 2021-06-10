3064493a5e368792117cfb5bcf8bf2d520971e9c
Anton Lazurko <63597446+antonlazurko@users.noreply.github.com>
2021-06-10 13:54:05 +0300

    add new version

--- src/Components/AppBar/AppBar.tsx
+++ src/Components/AppBar/AppBar.tsx
@@ -1,10 +1,13 @@
 import styles from "./AppBar.module.css";
 import Navigation from "../Navigation/Navigation";
+import { Thing } from "@antonlazurko/mylib";
+console.dir(Thing);
 
 export default function AppBar() {
   return (
     <div className={styles.AppBar}>
       <Navigation />
+      <Thing />
     </div>
   );
 }
